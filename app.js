00fd5edba1a7540aa46b8603771453c6bf6c1580
Janette Campbell <campbjp@hotmail.com>
2022-06-23 18:58:22 -0400

    added components and css

--- app.js
+++ app.js
@@ -1,4 +1,84 @@
-ReactDOM.render(
-  <h1>Commence Dashboard Creation!</h1>,
-  document.querySelector("main")
-);
+const Widget = () => {
+  return <h3>Widget</h3>;
+};
+
+const ReviewsSide = () => {
+  return <h3>Reviews</h3>;
+};
+
+const Customers = () => {
+  return <h3>Customers</h3>;
+};
+
+const OnlineAnalysis = () => {
+  return <h3>Online Analysis</h3>;
+};
+
+const Settings = () => {
+  return <h3>Settings</h3>;
+};
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <Widget />
+      <ReviewsSide />
+      <Customers />
+      <OnlineAnalysis />
+      <Settings />
+    </div>
+  );
+};
+
+const Reviews = () => {
+  return (
+    <div className="reviews">
+      <h3>Reviews</h3>
+      <h1>1,281</h1>
+    </div>
+  );
+};
+
+const AvgRating = () => {
+  return (
+    <div className="avg-rating">
+      <h3>Average Rating</h3>
+      <h1>4.6</h1>
+    </div>
+  );
+};
+
+const SentimentAnalysis = () => {
+  return (
+    <div className="sentiment-analysis">
+      <h3>Sentiment Analysis</h3>
+      <h2>960</h2>
+      <h2>122</h2>
+      <h2>321</h2>
+    </div>
+  );
+};
+
+const WebsiteVisitors = () => {
+  return (
+    <div className="website-visitors">
+      <h3>Website Visitors</h3>
+    </div>
+  );
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Dashboard />
+        <Reviews />
+        <AvgRating />
+        <SentimentAnalysis />
+        <WebsiteVisitors />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".main"));

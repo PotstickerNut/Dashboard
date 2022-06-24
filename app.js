ca1e5f5fa61500530b20364fe6ea5ba6698fa4c7
Janette Campbell <campbjp@hotmail.com>
2022-06-23 20:26:33 -0400

    added icons

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 const Widget = () => {
-  return <h3>Widget</h3>;
+  return (
+    <h3>
+      <img className="widgets" src="./img/widget-icon.png" />
+      Widget
+    </h3>
+  );
 };
 
 const ReviewsSide = () => {
-  return <h3>Reviews</h3>;
+  return (
+    <h3>
+      <img className="widgets" src="./img/star-icon.png" />
+      Reviews
+    </h3>
+  );
 };
 
 const Customers = () => {
-  return <h3>Customers</h3>;
+  return (
+    <h3>
+      <img className="widgets" src="./img/person-icon.png" />
+      Customers
+    </h3>
+  );
 };
 
 const OnlineAnalysis = () => {
-  return <h3>Online Analysis</h3>;
+  return (
+    <h3>
+      <img className="widgets" src="./img/analysis-icon.png" />
+      Online Analysis
+    </h3>
+  );
 };
 
 const Settings = () => {
-  return <h3>Settings</h3>;
+  return (
+    <h3>
+      <img className="widgets" src="./img/settings-icon.png" />
+      Settings
+    </h3>
+  );
 };
 
 const Dashboard = () => {
@@ -33,7 +58,11 @@ const Dashboard = () => {
 const Reviews = () => {
   return (
     <div className="reviews">
-      <h3>Reviews</h3>
+      <h3>
+        Reviews
+        <img className="menu" src="./img/menu-icon.png" />
+      </h3>
+
       <h1>1,281</h1>
     </div>
   );
@@ -42,7 +71,10 @@ const Reviews = () => {
 const AvgRating = () => {
   return (
     <div className="avg-rating">
-      <h3>Average Rating</h3>
+      <h3>
+        Average Rating
+        <img className="menu" src="./img/menu-icon.png" />
+      </h3>
       <h1>4.6</h1>
     </div>
   );
@@ -51,7 +83,10 @@ const AvgRating = () => {
 const SentimentAnalysis = () => {
   return (
     <div className="sentiment-analysis">
-      <h3>Sentiment Analysis</h3>
+      <h3>
+        Sentiment Analysis
+        <img className="menu" src="./img/menu-icon.png" />
+      </h3>
       <h2>960</h2>
       <h2>122</h2>
       <h2>321</h2>
@@ -62,7 +97,10 @@ const SentimentAnalysis = () => {
 const WebsiteVisitors = () => {
   return (
     <div className="website-visitors">
-      <h3>Website Visitors</h3>
+      <h3>
+        Website Visitors
+        <img className="menu" src="./img/menu-icon.png" />
+      </h3>
     </div>
   );
 };
